9f8212d70944d69dfb0a850ee6fc9e66a768fe0d
Manish <fhp450s@gmail.com>
2023-10-03 10:12:24 +0530

    fifth

--- hello/urls.py
+++ hello/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = "ZANAM FOUNDATION Admin"
 admin.site.site_title = "ZANAM FOUNDATION Admin Portal"
@@ -25,3 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
